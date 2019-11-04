90866e03776f50660137c47adee5af680935c978
Vyacheslav <myeve2007@yandex.ru>
2019-11-04 10:10:07 +0300

    feat: add common key events

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,12 @@ window.onload = function () {
     ];
 
     // set keyboard structure and styles
-    const RU = 1;
-    const EN = 3;
+    let RU = 1;
+    let EN = 3;
+    let language = 'ru';
+    let shiftBtn = false;
+    let capsLockBtn = false;
+
     for (let i = 1; i <= innerRowsCount; i++) {
         // add inner keyboard rows
         let keyboardRow = document.createElement('div');
@@ -67,8 +71,10 @@ window.onload = function () {
             let fillKeyText = function (language) {
                 if (!keyboardButton.classList.contains('system')) {
                     keyboardButton.textContent = value[language].toUpperCase();
+                    keyboardButton.setAttribute('data-value', value[language]);
                 } else {
                     keyboardButton.textContent = value[language];
+                    keyboardButton.setAttribute('data-value', '');
                 }
             };
 
@@ -93,41 +99,195 @@ window.onload = function () {
                     func();
                 });
 
-                document.addEventListener('keyup', function(e) {
+                document.addEventListener('keyup', (e) => {
                     pressed.delete(e.code);
                 });
             };
 
             languageChange(
-                () => keyboardButton.textContent.toLowerCase() === value[RU] ? fillKeyText(EN) : fillKeyText(RU),
+                () => {
+                    if(keyboardButton.getAttribute('data-value') === value[1] || keyboardButton.getAttribute('data-value') === value[3]) {
+                        keyboardButton.getAttribute('data-value') === value[1] ? fillKeyText(3) : fillKeyText(1);
+                    } else if (keyboardButton.getAttribute('data-value') === value[2] || keyboardButton.getAttribute('data-value') === value[4]) {
+                        keyboardButton.getAttribute('data-value') === value[2] ? fillKeyText(4) : fillKeyText(2);
+                    }
+                },
                 "AltLeft",
+                "ControlLeft"
+            );
+
+            // set change register option
+            let registerChange = function (func, ...codes) {
+                let pressed = new Set();
+
+                document.addEventListener('keydown', function(e) {
+                    pressed.add(e.code);
+                    // check if all buttons were pressed
+                    for (let code of codes) {
+                        if (!pressed.has(code)) {
+                            return;
+                        }
+                    }
+                    // to prevent key sticking - reset all key statuses
+                    pressed.clear();
+                    func();
+                });
+
+                document.addEventListener('keyup', (e) => {
+                    let unPressedBtnKeyCode = e.code;
+                    let keyCode = value[0];
+
+                    // weird language check
+                    if (keyCode === 'Backquote') {
+                        if (keyboardButton.getAttribute('data-value').toLowerCase() === 'ё') {
+                            language = 'ru'
+                        } else {
+                            language = 'en'
+                        }
+                    }
+                    if (unPressedBtnKeyCode === 'ShiftLeft' || unPressedBtnKeyCode === 'ShiftRight') {
+                        // return base lang settings if shift/lock was pressed
+                        RU = 1;
+                        EN = 3;
+                        shiftBtn = false;
+
+                        if (language === 'ru') {
+                            fillKeyText(RU);
+                        } else if (language === 'en') {
+                            fillKeyText(EN);
+                        }
+                    }
+                    pressed.delete(e.code);
+                });
+            };
+
+            registerChange(
+                () => {
+                    if (!shiftBtn) {
+                        if (language === 'ru') {
+                            RU = 2;
+                            keyboardButton.getAttribute('data-value') === value[1] ? fillKeyText(RU) : fillKeyText(1);
+                        } else if (language === 'en') {
+                            EN = 4;
+                            keyboardButton.getAttribute('data-value') === value[3] ? fillKeyText(EN) : fillKeyText(3);
+                        }
+                    }
+                },
                 "ShiftLeft"
+            );
+
+            registerChange(
+                () => {
+                    if (!shiftBtn) {
+                        RU = 2;
+                        EN = 4;
+                        if (language === 'ru') {
+                            keyboardButton.getAttribute('data-value') === value[1] ? fillKeyText(RU) : fillKeyText(1);
+                        } else if (language === 'en') {
+                            keyboardButton.getAttribute('data-value') === value[3] ? fillKeyText(EN) : fillKeyText(3);
+                        }
+                    }
+                },
+                "ShiftRight"
+            );
+
+            registerChange(
+                () => {
+                    capsLockBtn === true ? false : true;
+                    console.log(RU);
+                    if (language === 'ru') {
+                        RU = 2;
+                        keyboardButton.getAttribute('data-value') === value[1] ? fillKeyText(RU) : fillKeyText(1);
+                        console.log(RU);
+                    } else if (language === 'en') {
+                        EN = 4;
+                        keyboardButton.getAttribute('data-value') === value[3] ? fillKeyText(EN) : fillKeyText(3);
+                    }
+                },
+                "CapsLock"
             );
         });
     }
 
     // keyboard mouse events
+    let backspaceFn = () => document.querySelector('.textarea').value = document.querySelector('.textarea').value.slice(0, -1);
+    let tabFn = () => document.querySelector('.textarea').value += '    ';
+    let spaceFn = () => document.querySelector('.textarea').value += ' ';
+    let enterFn = () => document.querySelector('.textarea').value += '\n';
+    let deleteFn = () => {
+        let textCursorPositionStart = document.querySelector('.textarea').selectionStart;
+        let textCursorPositionEnd = document.querySelector('.textarea').selectionEnd;
+        let sliceNumber = textCursorPositionEnd - textCursorPositionStart;
+        if (textCursorPositionStart === textCursorPositionEnd) {
+            sliceNumber = 1;
+        }
+
+        document.querySelector('.textarea').value = document.querySelector('.textarea').value.slice(0, textCursorPositionStart)
+            + document.querySelector('.textarea').value.slice(textCursorPositionStart+sliceNumber);
+        document.querySelector('.textarea').selectionStart = textCursorPositionStart;
+        document.querySelector('.textarea').selectionEnd = textCursorPositionStart;
+    };
+
     document.querySelectorAll('.keyboard__key').forEach(function (value) {
         value.addEventListener('click', () => {
-            document.querySelector('.textarea').value += value.innerText;
+            document.querySelector('.textarea').value += value.getAttribute('data-value');
+            let pressedBtnKeyCode = value.getAttribute('data-key');
+
+            if (pressedBtnKeyCode === 'Backspace') {
+                backspaceFn();
+            }
+            if (pressedBtnKeyCode === 'Tab') {
+                tabFn();
+            }
+            if (pressedBtnKeyCode === 'Space') {
+                spaceFn();
+            }
+            if (pressedBtnKeyCode === 'Enter') {
+                enterFn();
+            }
+            if (pressedBtnKeyCode === 'Delete') {
+                deleteFn();
+            }
         });
     });
 
     // keyboard keys events
     document.addEventListener('keydown', (e) => {
+        e.preventDefault();
         document.querySelector('.textarea').focus();
         let pressedBtnKeyCode = e.code;
-        console.log(pressedBtnKeyCode);
 
         document.querySelectorAll('.keyboard__key').forEach((value) => {
             let keyCode = value.getAttribute('data-key');
             if (pressedBtnKeyCode === keyCode) {
                 value.classList.add('active');
+                document.querySelector('.textarea').value += value.getAttribute('data-value');
+            }
+            if (pressedBtnKeyCode === keyCode && keyCode === 'Backspace') {
+                backspaceFn();
+            }
+            if (pressedBtnKeyCode === keyCode && keyCode === 'Tab') {
+                tabFn();
+            }
+            if (pressedBtnKeyCode === keyCode && keyCode === 'Space') {
+                spaceFn();
+            }
+            if (pressedBtnKeyCode === keyCode && keyCode === 'Enter') {
+                enterFn();
+            }
+            if (pressedBtnKeyCode === keyCode && keyCode === 'Delete') {
+                deleteFn();
+            }
+            if (pressedBtnKeyCode === keyCode && (keyCode === 'ShiftLeft' || keyCode === 'ShiftRight') && !shiftBtn) {
+                shiftBtn = true;
+            }
+            if (pressedBtnKeyCode === keyCode && keyCode === 'CapsLock' && !capsLockBtn) {
+                capsLockBtn = true;
             }
         });
     });
 
-    document.addEventListener('keyup', (e) => {
+    document.addEventListener('keyup', () => {
         document.querySelectorAll('.keyboard__key').forEach((value) => {
             value.classList.remove('active');
         });
